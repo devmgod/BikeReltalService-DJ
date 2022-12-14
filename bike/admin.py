e2ccd351988d7ed66b1e829a3639fc604e7c8727
Tykhon <devmgod0112@gmail.com>
2022-12-14 20:12:46 +0900

    finish-db-model

--- bike/admin.py
+++ bike/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Bike, Rating, Reservation, User
+from .models import Bike, Rating, Reservation, User, Role
 
 admin.site.register(User)
 admin.site.register(Bike)
 admin.site.register(Rating)
 admin.site.register(Reservation)
+admin.site.register(Role)
--- bike/models.py
+++ bike/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 class User(AbstractUser):
-    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=False)
     email = models.EmailField(unique=True, null=False)
 
     avatar = models.ImageField(null=True, default='avatar.svg')
+    role = models.ManyToManyField(Role, related_name='position', blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -17,6 +25,8 @@ class Bike(models.Model):
     location = models.CharField(max_length=200, null=True)
     
     status = models.BooleanField(default=False)
+
+    amount = models.IntegerField(null=True)
 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -43,7 +53,7 @@ class Rating(models.Model):
 
 class Reservation(models.Model):
     host = models.ForeignKey(User, on_delete=models.CASCADE)
-    bike_model = models.ForeignKey(Bike, on_delete=models.CASCADE)
+    bike_model = models.ForeignKey(Bike, on_delete=models.SET_NULL, null=True)
     amount = models.IntegerField(null=False)
 
     updated = models.DateTimeField(auto_now=True)
